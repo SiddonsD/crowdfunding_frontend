b72c734a0a67ff3a71df44667feccbb564191851
SiddonsD <denise.siddons@gmail.com>
2024-02-29 08:18:30 +0800

    updates navbar behaviour on logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,17 +5,20 @@ function NavBar() {
     const {auth, setAuth} = useAuth();
 
     const handleLogout = () => {
-        window.localStorage.removeItem("token");
+        event.preventDefault(); // prvents default link navigation
+        window.localStorage.removeItem("token"); // removes auth token when user logs out
         setAuth({ token: null });
+        Navigate('/'); // navigates to home page post logout
     };
 
-    console.log(auth)
+    // console.log(auth)
 
     return (
         <div>
             <nav>
                 <Link to="/">Home</Link>
                 {auth.token ? (
+                    // group links without extra nodes to DOM
                     <>
                     <Link to="/projects/create">Create Project</Link>
                     <Link to="/" onClick={handleLogout}>Log Out</Link>
