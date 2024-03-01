847db40b49d5b28bda3e629a719ef7b0074268e1
SiddonsD <denise.siddons@gmail.com>
2024-03-01 10:45:26 +0800

    add project stats to project page

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -12,6 +12,20 @@ function ProjectPage() {
     if (error) return <h1>{error.message}</h1>;
     if (!project) return <h1>Project not found!</h1>;
 
+    // calculate total pledges
+    const totalPledged = project.pledges.reduce((total, pledge) => total + pledge.amount, 0)
+
+    // calculate num pledges made as supporters 
+    const totalSupporters = project.pledges.length;
+
+    // calculate num campaign days left
+    const currentDate = new Date();
+    const endDate = new Date(project.end_date);
+    const timeDiff = endDate.getTime() - currentDate.getTime();
+    const daysLeft = Math.ceil.ceil(timeDiff / (1000 * 3600 * 24));
+
+    // calculate percentage of goal raised
+    const goalPercentage = Math.min((totalPledged / project.goal) * 100, 100);
     // convert is_open to active/inactive
     const getStatusText = (is_open) => is_open ? 'Active' : 'Inactive'
 
@@ -57,6 +71,12 @@ function ProjectPage() {
         <h3>Campaign Start Date: {formatDate(project.date_created)}</h3>
         <h3>Status: {getStatusText(project.is_open)}</h3>
         <h3>Description: {project.description}</h3>
+        <h3>Goal: {formatCurrency(project.goal)}</h3>
+        <h3>Total Pledged: {formatCurrency(totalPledged)}</h3>
+        <h3>Days Left: {daysLeft > 0 ? daysLeft: 'Campaign ended.'}</h3>
+        <div className="progress-bar-container">
+            <div className="progress-bar" style={{ width: `${goalPercentage}%`}}></div>
+        </div>
         <h3>Pledges:</h3>
         <ul>
             {sortedPledges.map((pledgeData, key) => {
